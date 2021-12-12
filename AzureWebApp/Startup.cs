2cc01d7d5916a063b141988b6b41494a0417b37a
Migs-GIT <miguelitobaluyot29@gmail.com>
2021-12-12 10:17:36 +0800

    sample dot net

---
+++ AzureWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebApp.Startup))]
+namespace AzureWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
